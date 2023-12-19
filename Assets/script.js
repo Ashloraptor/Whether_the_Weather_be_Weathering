bcd673efc28bf4ffd68b4afdd426d763cf275750
Ash <ashloraptor@gmail.com>
2023-12-19 12:17:07 -0500

    Got mad that I couldn't get the form to work and deleted all JavaScript except for a few variables.

---
+++ Assets/script.js
@@ -0,0 +1,4 @@
+//var searchHistory = [];
+var weatherAPIRootUrl = "https://api.openweathermap.org/data/2.5/weather";
+var APIKey = "3ade81378253fcb432210516a1fb62b3";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
