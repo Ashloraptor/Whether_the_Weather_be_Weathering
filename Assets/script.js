7764dab7a0f42778fb2a6afdfe1e67c94079a2b9
Ash <ashloraptor@gmail.com>
2023-12-20 21:25:12 -0500

    Modified both HTML and JavaScript so that the form creates a value with the data inputted by user.

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,22 @@
 //var searchHistory = [];
 var weatherAPIRootUrl = "https://api.openweathermap.org/data/2.5/weather";
-var APIKey = "3ade81378253fcb432210516a1fb62b3";
+//var APIKey = ;
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+var searchForm = document.getElementById("search-form");
+var searchInput = document.getElementById("search-input");
+// Function to log the search input value
+function searchCurrentWeather(city) {
+
+}
+function logMessage(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+    // Retrieve the input value
+    var inputValue = searchInput.value.trim();
+    // Log the message to the console
+    console.log("You searched for: " + inputValue);
+    //callback to search function (the one that does the fetch request). Include parameter "input value"
+    searchCurrentWeather(inputValue);
+  }
+
+searchForm.addEventListener("submit", logMessage);
